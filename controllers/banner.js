e3dcfa8563d4984c22af617312b302232a9e4669
UmarFaruqueN <umarfaruquen@gmail.com>
2022-03-31 15:21:55 +0530

    banner managemnt

--- controllers/banner.js
+++ controllers/banner.js
@@ -31,11 +31,11 @@ module.exports = {
           }
      },
      getBanner: async (req, res) => {
-        console.log(req.body); 
-          const {bannerFor} = req.body
-          console.log(req.body.bannerFor+"started getbanner");
+          console.log(req.body);
+          const { bannerFor } = req.body;
+          console.log(req.body.bannerFor + "started getbanner");
           try {
-               const allBanner = await Banner.find({for:bannerFor});
+               const allBanner = await Banner.find({ for: bannerFor });
                console.log(allBanner);
                if (allBanner) {
                     return res.status(200).json({ message: " Banner fetched Successfully", allBanner });
@@ -45,5 +45,17 @@ module.exports = {
                return res.status(500).json({ message: "something went wrong" });
           }
      },
-     deleteBanner: async (req, res) => {},
+     deleteBanner: async (req, res) => {
+          console.log("on delete banner Controller");
+          console.log(req.body);
+          const { _id } = req.body;
+          try {
+               const DeleteBanner = await Banner.deleteOne({ _id: ObjectId(_id) });
+               if (DeleteBanner) return res.status(200).json({ message: " Banner Deleted" });
+               return res.status(500).json({ message: "Banner Not Deleted" });
+          } catch (error) {
+               console.log(error.message + "entho error");
+               return res.status(500).json({ message: "something went wrong" });
+          }
+     },
 };
